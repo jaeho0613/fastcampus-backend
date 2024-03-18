28721de612c4cc443ec23fcfbe6633f281442556
jaeho0613 <ywm2004@gmail.com>
2024-03-19 00:08:25 +0900

    Part1. Ch06 완료

---
+++ mvc-practice2/src/main/java/org/example/SimpleControllerHandlerAdapter.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.mvc.HandlerAdapter;
+import org.example.mvc.ModelAndView;
+import org.example.mvc.controller.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleControllerHandlerAdapter implements HandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof Controller);
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String viewName = ((Controller) handler).handleRequest(request, response);
+        return new ModelAndView(viewName);
+    }
+}
